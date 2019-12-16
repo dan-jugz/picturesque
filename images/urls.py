de023a95e382810c1f5420d7279288ba7bfaee06
daniel njuguna <njugunadaniel364@gmail.com>
2019-12-16 10:17:30 +0300

    setup the route for viewing a full image on clicking

--- images/urls.py
+++ images/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
   path('',views.home,name='home'),
   re_path('^search_results$',views.search_results,name='search'),
+  re_path('^fullimage/(\d+)',views.fullimage,name='fullimage'),
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
