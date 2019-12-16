d163703ebf6811258b00c36caa5b35ae0a5e62b7
daniel njuguna <njugunadaniel364@gmail.com>
2019-12-16 10:21:52 +0300

    write a method that returns the image clicked on with details

--- images/views.py
+++ images/views.py
@@ -18,3 +18,10 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render (request,'searched.html',{"message":message})
+
+
+
+def fullimage(request,image_id):
+    the_image = Image.objects.get(id = image_id)
+    
+    return render(request,'fullimage.html',{"image":the_image})
