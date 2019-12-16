dc1e11a36f774fdb5c9fc79749fe3b7751c3cd3d
daniel njuguna <njugunadaniel364@gmail.com>
2019-12-16 10:47:20 +0300

    write the tests for the image model  and to check for saving

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 
-# Create your tests here.
+class TestModelImages(TestCase):
+
+  def setUp(self):
+    self.categ = Category(category = "travels")
+    self.categ.save()
+    self.locate = Location(location = "mombasa",date_taken = "2019-10-04")
+    self.locate.save()
+    self.new_image = Image(image = "tesla.png",image_name = "tesla",image_description = "cool picture",image_post_date = "2019-10-o4",image_photographer = "denis",category = self.categ,location = self.locate)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+
+    def test_save_image(self):
+        self.new_image.save_image()
+        image_length = Image.objects.all()
+        self.assertTrue(len(image_length) > 0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+
+
